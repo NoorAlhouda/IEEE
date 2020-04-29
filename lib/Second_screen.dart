aad1d6c50106bc3e858907f72af30aef72c8f5d5
nour Alhouda <simia.alafandy@gmail.com>
2020-04-29 16:17:27 +0200

    first commit

---
+++ lib/Second_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'splash_demo.dart';
+
+class SecondScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SecondScreen();
+  }
+}
+
+class _SecondScreen extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'splash_demo.dart';
+import 'Second_screen.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashDemo(),
+    ));
